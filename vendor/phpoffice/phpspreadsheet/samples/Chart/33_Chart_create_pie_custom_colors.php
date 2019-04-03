e7d92dbd17d79267477b552a0d9f435bfe6dfdc8
DavidZ <zirhut@gmail.com>
2019-04-03 13:40:56 +0200

    Upgrade PhpSpreadSheet to 1.6.0

--- vendor/phpoffice/phpspreadsheet/samples/Chart/33_Chart_create_pie_custom_colors.php
+++ vendor/phpoffice/phpspreadsheet/samples/Chart/33_Chart_create_pie_custom_colors.php
@@ -3,6 +3,7 @@
 use PhpOffice\PhpSpreadsheet\Chart\Chart;
 use PhpOffice\PhpSpreadsheet\Chart\DataSeries;
 use PhpOffice\PhpSpreadsheet\Chart\DataSeriesValues;
+use PhpOffice\PhpSpreadsheet\Chart\Layout;
 use PhpOffice\PhpSpreadsheet\Chart\Legend;
 use PhpOffice\PhpSpreadsheet\Chart\PlotArea;
 use PhpOffice\PhpSpreadsheet\Chart\Title;
@@ -23,6 +24,11 @@ $worksheet->fromArray(
         ]
 );
 
+// Custom colors for dataSeries (gray, blue, red, orange)
+$colors = [
+    'cccccc', '00abb8', 'b8292f', 'eb8500',
+];
+
 // Set the Labels for each data series we want to plot
 //     Datatype
 //     Cell reference for data
@@ -31,9 +37,7 @@ $worksheet->fromArray(
 //     Data values
 //     Data Marker
 $dataSeriesLabels1 = [
-    new DataSeriesValues(DataSeriesValues::DATASERIES_TYPE_STRING, 'Worksheet!$B$1', null, 1), // 2010
     new DataSeriesValues(DataSeriesValues::DATASERIES_TYPE_STRING, 'Worksheet!$C$1', null, 1), // 2011
-    new DataSeriesValues(DataSeriesValues::DATASERIES_TYPE_STRING, 'Worksheet!$D$1', null, 1), // 2012
 ];
 // Set the X-Axis Labels
 //     Datatype
@@ -52,29 +56,32 @@ $xAxisTickValues1 = [
 //     Number of datapoints in series
 //     Data values
 //     Data Marker
+//     Custom colors
 $dataSeriesValues1 = [
-    new DataSeriesValues(DataSeriesValues::DATASERIES_TYPE_NUMBER, 'Worksheet!$B$2:$B$5', null, 4),
-    new DataSeriesValues(DataSeriesValues::DATASERIES_TYPE_NUMBER, 'Worksheet!$C$2:$C$5', null, 4),
-    new DataSeriesValues(DataSeriesValues::DATASERIES_TYPE_NUMBER, 'Worksheet!$D$2:$D$5', null, 4),
+    new DataSeriesValues(DataSeriesValues::DATASERIES_TYPE_NUMBER, 'Worksheet!$C$2:$C$5', null, 4, [], null, $colors),
 ];
 
 // Build the dataseries
 $series1 = new DataSeries(
-    DataSeries::TYPE_AREACHART, // plotType
-    DataSeries::GROUPING_PERCENT_STACKED, // plotGrouping
+    DataSeries::TYPE_PIECHART, // plotType
+    null, // plotGrouping (Pie charts don't have any grouping)
     range(0, count($dataSeriesValues1) - 1), // plotOrder
     $dataSeriesLabels1, // plotLabel
     $xAxisTickValues1, // plotCategory
     $dataSeriesValues1          // plotValues
 );
 
-// Set the series in the plot area
-$plotArea1 = new PlotArea(null, [$series1]);
-// Set the chart legend
-$legend1 = new Legend(Legend::POSITION_TOPRIGHT, null, false);
+// Set up a layout object for the Pie chart
+$layout1 = new Layout();
+$layout1->setShowVal(true);
+$layout1->setShowPercent(true);
 
-$title1 = new Title('Test %age-Stacked Area Chart');
-$yAxisLabel1 = new Title('Value ($k)');
+// Set the series in the plot area
+$plotArea1 = new PlotArea($layout1, [$series1]);
+// Set the chart legend
+$legend1 = new Legend(Legend::POSITION_RIGHT, null, false);
+
+$title1 = new Title('Test Pie Chart');
 
 // Create the chart
 $chart1 = new Chart(
@@ -85,7 +92,7 @@ $chart1 = new Chart(
     true, // plotVisibleOnly
     0, // displayBlanksAs
     null, // xAxisLabel
-    $yAxisLabel1 // yAxisLabel
+    null   // yAxisLabel - Pie charts don't have a Y-Axis
 );
 
 // Set the position where the chart should appear in the worksheet
@@ -103,9 +110,7 @@ $worksheet->addChart($chart1);
 //     Data values
 //     Data Marker
 $dataSeriesLabels2 = [
-    new DataSeriesValues(DataSeriesValues::DATASERIES_TYPE_STRING, 'Worksheet!$B$1', null, 1), // 2010
     new DataSeriesValues(DataSeriesValues::DATASERIES_TYPE_STRING, 'Worksheet!$C$1', null, 1), // 2011
-    new DataSeriesValues(DataSeriesValues::DATASERIES_TYPE_STRING, 'Worksheet!$D$1', null, 1), // 2012
 ];
 // Set the X-Axis Labels
 //     Datatype
@@ -124,43 +129,42 @@ $xAxisTickValues2 = [
 //     Number of datapoints in series
 //     Data values
 //     Data Marker
+//     Custom colors
 $dataSeriesValues2 = [
-    new DataSeriesValues(DataSeriesValues::DATASERIES_TYPE_NUMBER, 'Worksheet!$B$2:$B$5', null, 4),
-    new DataSeriesValues(DataSeriesValues::DATASERIES_TYPE_NUMBER, 'Worksheet!$C$2:$C$5', null, 4),
-    new DataSeriesValues(DataSeriesValues::DATASERIES_TYPE_NUMBER, 'Worksheet!$D$2:$D$5', null, 4),
+    $dataSeriesValues2Element = new DataSeriesValues(DataSeriesValues::DATASERIES_TYPE_NUMBER, 'Worksheet!$C$2:$C$5', null, 4),
 ];
+$dataSeriesValues2Element->setFillColor($colors);
 
 // Build the dataseries
 $series2 = new DataSeries(
-    DataSeries::TYPE_BARCHART, // plotType
-    DataSeries::GROUPING_STANDARD, // plotGrouping
+    DataSeries::TYPE_DONUTCHART, // plotType
+    null, // plotGrouping (Donut charts don't have any grouping)
     range(0, count($dataSeriesValues2) - 1), // plotOrder
     $dataSeriesLabels2, // plotLabel
     $xAxisTickValues2, // plotCategory
     $dataSeriesValues2        // plotValues
 );
-// Set additional dataseries parameters
-//     Make it a vertical column rather than a horizontal bar graph
-$series2->setPlotDirection(DataSeries::DIRECTION_COL);
+
+// Set up a layout object for the Pie chart
+$layout2 = new Layout();
+$layout2->setShowVal(true);
+$layout2->setShowCatName(true);
 
 // Set the series in the plot area
-$plotArea2 = new PlotArea(null, [$series2]);
-// Set the chart legend
-$legend2 = new Legend(Legend::POSITION_RIGHT, null, false);
+$plotArea2 = new PlotArea($layout2, [$series2]);
 
-$title2 = new Title('Test Column Chart');
-$yAxisLabel2 = new Title('Value ($k)');
+$title2 = new Title('Test Donut Chart');
 
 // Create the chart
 $chart2 = new Chart(
     'chart2', // name
     $title2, // title
-    $legend2, // legend
+    null, // legend
     $plotArea2, // plotArea
     true, // plotVisibleOnly
     0, // displayBlanksAs
     null, // xAxisLabel
-    $yAxisLabel2 // yAxisLabel
+    null   // yAxisLabel - Like Pie charts, Donut charts don't have a Y-Axis
 );
 
 // Set the position where the chart should appear in the worksheet
